78a7659243833590834cc118047d10108c42ec28
Karthik <kb.karthick97@gmail.com>
2024-05-10 04:58:51 +0530

    First commit : Added springBoot dependencies

---
+++ src/test/java/com/ECommerce/FakeStoreApplication/FakeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ECommerce.FakeStoreApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
